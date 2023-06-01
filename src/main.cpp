80229ed7ef0faaa79c0d05c0eada16f971ac6ff6
Paulius Ragauskas <pauliusrag@gmail.com>
2023-06-01 19:16:07 +0300

    feat: add yahoo api parsing example

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <curl.h>
+#include <string>
 
-using namespace std;
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* data) {
+    size_t totalSize = size * nmemb;
+    data->append(static_cast<char*>(contents), totalSize);
+    return totalSize;
+}
 
-void test_curl(void) {
-    CURL *curl;
+int main() {
+    CURL* curl;
     CURLcode res;
+    std::string data;
 
     curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://google.com");
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://query1.finance.yahoo.com/v8/finance/chart/TSLA?interval=1d");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+
         res = curl_easy_perform(curl);
-        // printf("vaziuojam\r");
-        /* always cleanup */
+        if (res != CURLE_OK) {
+            std::cerr << "Failed to fetch URL: " << curl_easy_strerror(res) << std::endl;
+        }
+
         curl_easy_cleanup(curl);
     }
-}
 
-int main()
-{
-    cout<<"Hello World";
-    int test = 5;
-    float kan = 2.5;
-    test = kan;
-    test_curl();
+    // Display the fetched data
+    std::cout << "Fetched data:\n" << data << std::endl;
+
     return 0;
 }
-
